b516ea47519e7abff178f03500437f70b7563bef
Ренат Ренат <renat.churbanov@gmail.com>
2022-05-30 21:41:56 +0300

    Добавлен контроллера для сущности Client Добавлено создание клиента

---
+++ Gymopedia/Controllers/ClientController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gymopedia.Core.Models;
+using Gymopedia.Core.Clients;
+using MediatR;
+
+namespace Gymopedia.Controllers
+{
+    [Route("/client")]
+    [ApiController]
+    public class ClientController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ClientController (IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("/create")]
+        public async Task<ClientDto> Create(CreateClient.Request request, CancellationToken cancellationToken)
+        {
+            var createClientResponse = await _mediator.Send(request, cancellationToken);
+
+            return new ClientDto
+            {
+                Name = createClientResponse.Client.Name,
+                CoachIds = createClientResponse.Client.CoachIds
+            };
+        }
+    }
+}
